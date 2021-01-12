ca521e7e2319c7a988933a014f961aa088d3f5d0
Technical-Jigar <tushalverma69@gmail.com>
2021-01-12 15:08:49 +0530

    first commit

---
+++ config.py
@@ -0,0 +1,10 @@
+from tobrot.sample_config import Config
+#Fill your all data, read readme for reference
+
+class Config(Config):
+	TG_BOT_TOKEN= "141234:1A8B3hV2leKOE6waNwo"
+	APP_ID = 7284610
+	API_HASH = "52916hf8476hgg772ksbxo72972a938"
+	OWNER_ID = 1845927413 #ID of bot owner
+	AUTH_CHANNEL = [-1001339069584]
+	
